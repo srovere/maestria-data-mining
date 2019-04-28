816b8019bd9f28403eb4dbb3ec1609dd90bf4f89
Santiago Rovere <srovere@gmail.com>
2019-04-28 11:03:34 -0300

    Exploración de parámetros

--- src/python/aprendizaje-automatico/TP1/experimentacion.py
+++ src/python/aprendizaje-automatico/TP1/experimentacion.py
@@ -18,17 +18,17 @@ target   = setDatosDiscretizado['cardio']
 trainValidationFeatures, testFeatures, trainValidationTarget, testTarget = \
     sklearn.model_selection.train_test_split(features, target, train_size = 0.7, test_size = 0.3, random_state = 0)
 trainFeatures, validationFeatures, trainTarget, validationTarget = \
-    sklearn.model_selection.train_test_split(trainValidationFeatures, trainValidationTarget, train_size = 0.6, test_size = 0.4, random_state = 0)
+    sklearn.model_selection.train_test_split(trainValidationFeatures, trainValidationTarget, train_size = 0.7, test_size = 0.3, random_state = 0)
 
 # Ajustar set de entrenamiento
 decisionTree = sklearn.tree.DecisionTreeClassifier(max_depth = 5, criterion = "gini", random_state = 0)
 decisionTree.fit(trainFeatures, trainTarget)
 
 # Evaluar precisión
-predictions = decisionTree.predict(testFeatures)
-confusionMatrix = sklearn.metrics.confusion_matrix(testTarget, predictions)
+predictions = decisionTree.predict(validationFeatures)
+confusionMatrix = sklearn.metrics.confusion_matrix(validationTarget, predictions)
 print(confusionMatrix)
-accuracy = sklearn.metrics.accuracy_score(testTarget, predictions)
+accuracy = sklearn.metrics.accuracy_score(validationTarget, predictions)
 print(accuracy)
 
 # Graficado del árbol
@@ -36,3 +36,21 @@ dotFile = sklearn.externals.six.StringIO()
 sklearn.tree.export_graphviz(decisionTree, out_file = dotFile, filled = True, rounded = True, special_characters = True)
 graph = pydotplus.graph_from_dot_data(dotFile.getvalue())
 graph.write_png("output/DT.png")
+
+# Ahora hago una exploración de parámetros para optimizar el árbol
+parameters = { 'max_depth': range(1, 9), 'criterion':  [ 'entropy', 'gini' ] }
+gridSearch = sklearn.model_selection.GridSearchCV(sklearn.tree.DecisionTreeClassifier(), parameters, n_jobs = 4)
+gridSearch.fit(X = trainValidationFeatures, y = trainValidationTarget)
+treeModel  = gridSearch.best_estimator_
+print (gridSearch.best_score_, gridSearch.best_params_)
+
+# Evaluar precisión con set de testeo
+predictions = treeModel.predict(testFeatures)
+accuracy = sklearn.metrics.accuracy_score(testTarget, predictions)
+print(accuracy)
+
+# Graficado del árbol óptimo
+dotFile = sklearn.externals.six.StringIO()
+sklearn.tree.export_graphviz(decisionTree, out_file = dotFile, filled = True, rounded = True, special_characters = True)
+graph = pydotplus.graph_from_dot_data(dotFile.getvalue())
+graph.write_png("output/DT-optimal.png")
