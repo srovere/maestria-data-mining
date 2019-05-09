edd7347f0f5822d33b914d6026e7e7b7187e197f
Santiago Rovere <srovere@gmail.com>
2019-05-09 14:30:14 -0300

    Analisis de faltantes

--- src/python/aprendizaje-automatico/TP1/analisis.py
+++ src/python/aprendizaje-automatico/TP1/analisis.py
@@ -6,26 +6,30 @@
 import numpy
 import pandas
 import pydotplus
+import random
 import sklearn.externals.six
 import sklearn.metrics
 import sklearn.model_selection
 import sklearn.tree
+import sys
 
 # Definicion de funcion para discretizar variables por cuantiles
 def DiscretizarPorCuantiles(datos, columnas, intervalos):
     datosNuevos = datos.copy()
+    informacion = {}
     for columna in columnas:
         paso                 = 1 / intervalos
         cuantiles            = numpy.arange(0, 1.0 + paso, paso)
         etiquetas            = list(map(str, numpy.arange(1, intervalos + 1)))
         categorias           = pandas.qcut(x=datos[columna], q=cuantiles, labels=etiquetas)
         datosNuevos[columna] = categorias
-    return datosNuevos
+        informacion[columna] = { "cuantiles": cuantiles, "etiquetas": etiquetas }
+    return [datosNuevos, informacion ]
 
 # Lectura de set de datos discretizado
-setDatosOriginal     = pandas.read_csv(filepath_or_buffer = "input/SetDatosDiscretizado.csv", sep = "\t",
+setDatosOriginal = pandas.read_csv(filepath_or_buffer = "input/SetDatosDiscretizado.csv", sep = "\t",
                                        na_values = "NA")
-setDatosDiscretizado = DiscretizarPorCuantiles(datos=setDatosOriginal, columnas=['age','height','weight'], intervalos=10)
+setDatosDiscretizado, informacion = DiscretizarPorCuantiles(datos=setDatosOriginal, columnas=['age','height','weight'], intervalos=10)
 
 # Separo los features del data frame del target a aprender
 atributos = setDatosDiscretizado.drop(columns = [ "id", "cardio" ])
@@ -43,6 +47,7 @@ atributosDesarrollo, atributosTest, objetivoDesarrollo, objetivoTest = \
 
 def EntrenarYEvaluarPerformance(atributos, objetivo, parametros, metricas, metrica_mejor_ajuste, k=5):
     # Entrenar y calcular metricas
+    random.seed(0)
     gridSearch = sklearn.model_selection.GridSearchCV(estimator=sklearn.tree.DecisionTreeClassifier(),
                                                       param_grid=parametros, cv=k, n_jobs=2, scoring=metricas,
                                                       return_train_score=True, refit=metrica_mejor_ajuste)
@@ -59,14 +64,15 @@ def EntrenarYEvaluarPerformance(atributos, objetivo, parametros, metricas, metri
             datosMetrica.append(metrica)
             for i in range(0, k):
                 keyName = "split" + str(i) + "_" + tipoSet + "_" + metrica
-                valores = resultados[keyName]
-                for j in range(0, len(combinaciones)):
-                    datosCombinacion = datosMetrica.copy()
-                    datosCombinacion.append(i)
-                    datosCombinacion.append(valores[j])
-                    for parametro in parametros.keys():
-                        datosCombinacion.append(combinaciones[j][parametro])
-                    resultadosDF.append(datosCombinacion)
+                if keyName in resultados:
+                    valores = resultados[keyName]
+                    for j in range(0, len(combinaciones)):
+                        datosCombinacion = datosMetrica.copy()
+                        datosCombinacion.append(i)
+                        datosCombinacion.append(valores[j])
+                        for parametro in parametros.keys():
+                            datosCombinacion.append(combinaciones[j][parametro])
+                        resultadosDF.append(datosCombinacion)
 
     columnas  = ["conjunto", "metrica", "k", "valor"]
     columnas.extend(parametros.keys())
@@ -105,6 +111,90 @@ gridSearch, resultados = EntrenarYEvaluarPerformance(atributos=atributosDesarrol
                                                      metrica_mejor_ajuste='roc_auc', k=5)
 print(resultados)
 print("Profundidad del mejor árbol: " + str(gridSearch.best_estimator_.tree_.max_depth))
+
+# 2.3 Tomando el mejor árbol del punto 2.2 (criterio = Gini) para hacer algunos
+#     experimentos que permitan testear la tolerancia a faltantes.
+
+# Definicion de función para calcular la moda de una lista de valores
+def Moda(valores):
+    moda = max(set(valores), key=valores.count)
+    return moda
+
+# Definición de función para agregar una cierta cantidad de faltantes a una columna
+def AgregarFaltantes(atributos, tasaFaltantes, columna):
+    # Determinar el tamano de la población y de la muestra
+    tamanoPoblacion = atributos.shape[0]
+    tamanoMuestra   = round(tasaFaltantes * tamanoPoblacion)
+
+    # Seleccionar al azar una cantidad filas
+    random.seed(0)
+    filas   = list(numpy.arange(0, tamanoPoblacion))
+    muestra = random.sample(filas, tamanoMuestra)
+
+    # Para la columna indicada, poner como faltantes los valores de las filas elegidas
+    atributosConFaltantes = atributos.copy()
+    atributosConFaltantes.loc[list(elem in muestra for elem in filas), columna] = None
+    return atributosConFaltantes
+
+# Definicion de función para reeemplazar faltantes
+def ReemplazarFaltantes(atributos, columna, objetivo=None):
+    if objetivo is None:
+        # Moda común
+        valoresColumna = list(atributos[atributos[columna].notnull()][columna])
+        modaColumna    = Moda(valoresColumna)
+        atributosNuevo = atributos.copy()
+        atributosNuevo[columna].fillna(modaColumna, inplace=True)
+        return atributosNuevo
+    else:
+        # Moda por clase
+        clases         = list(objetivo.unique())
+        filasNaN       = numpy.logical_not(list(atributos[columna].notnull()))
+        atributosNuevo = atributos.copy()
+        for clase in clases:
+            filasClase     = list(objetivo == clase)
+            filasNaNClase  = list(numpy.logical_and(filasNaN, filasClase))
+            atributosClase = atributos.loc[filasClase,]
+            valoresColumna = list(atributosClase[atributosClase[columna].notnull()][columna])
+            modaColumna    = Moda(valoresColumna)
+            atributosNuevo.loc[filasNaNClase, columna] = modaColumna
+        return atributosNuevo
+
+# Separar el set de desarrollo en sets de entrenamiento y validacion
+atributosEntrenamiento, atributosValidacion, objetivoEntrenamiento, objetivoValidacion = \
+    sklearn.model_selection.train_test_split(atributosDesarrollo, objetivoDesarrollo,
+                                             train_size = 0.8, test_size = 0.2, random_state = 0)
+
+# Generar sets de datos con tasas de faltantes de 0 a 0.8 con un paso de 0.05
+# Reemplazarlos por la moda y moda de clase.
+# Ajustar el árbol de decisión con Gini y altura 6 (el mejor árbol de 2.2)
+analisisSensibilidadFaltantes = []
+for clase in [ 'Moda', 'Moda de clase' ]:
+    for tasaFaltantes in [0, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8]:
+        print("Experimentando con ", clase, " y ", str(tasaFaltantes))
+        atributosEntrenamientoSinFaltantes = atributosEntrenamiento
+        if (tasaFaltantes > 0):
+            atributosEntrenamientoConFaltantes = AgregarFaltantes(atributos=atributosEntrenamiento,
+                                                                  tasaFaltantes=tasaFaltantes,
+                                                                  columna="weight")
+            if clase == "Moda":
+                atributosEntrenamientoSinFaltantes = ReemplazarFaltantes(atributos=atributosEntrenamientoConFaltantes,
+                                                                         columna="weight")
+            else:
+                atributosEntrenamientoSinFaltantes = ReemplazarFaltantes(atributos=atributosEntrenamientoConFaltantes,
+                                                                         columna="weight",
+                                                                         objetivo=objetivoEntrenamiento)
+
+        arbolDecision   = sklearn.tree.DecisionTreeClassifier(criterion = "gini", random_state = 0)
+        arbolDecision.fit(atributosEntrenamientoSinFaltantes, objetivoEntrenamiento)
+        predicciones    = arbolDecision.predict(atributosValidacion)
+        accuracy        = sklearn.metrics.accuracy_score(objetivoValidacion, predicciones)
+        profundidad     = arbolDecision.tree_.max_depth
+        analisisSensibilidadFaltantes.append([ tasaFaltantes, clase, profundidad, accuracy ])
+
+analisisSensibilidadFaltantes = pandas.DataFrame.from_records(analisisSensibilidadFaltantes,
+                                                              columns=["faltantes", "relleno", "profundidad", "accuracy"])
+
+# def EntrenarYEvaluarPerformance(atributos, objetivo, parametros, metricas, metrica_mejor_ajuste, k=5):
 
 # Ajustar set de entrenamiento
 # decisionTree = sklearn.tree.DecisionTreeClassifier(max_depth = 5, criterion = "gini", random_state = 0)
