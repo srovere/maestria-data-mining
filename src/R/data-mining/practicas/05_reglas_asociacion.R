595ad377bdbc968dc2d292c3c50b2899eda44956
Santiago Rovere <srovere@gmail.com>
2019-06-04 16:41:45 -0300

    Primera practica de reglas de asociacion

---
+++ src/R/data-mining/practicas/05_reglas_asociacion.R
@@ -0,0 +1,55 @@
+# Limpiar ambiente
+rm(list = objects())
+
+# Cargar paquetes
+require(dplyr)
+require(rlang)
+
+# Cargar dataset
+load("input/LAB05/titanic.raw.rdata")
+
+# Funcion para calcular soporte
+Contar    <- function(datos, itemset) {
+  for (col.name in names(itemset)) {
+    if (! is.null(itemset[[col.name]]) && (length(itemset[[col.name]]) > 0)) {
+      field.name <- rlang::sym(col.name)
+      if (length(itemset[[col.name]]) > 1) {
+        field.values <- itemset[[col.name]]
+        datos        <- dplyr::filter(datos, UQ(field.name) %in% field.values)
+      } else {
+        field.value <- itemset[[col.name]]
+        datos       <- dplyr::filter(datos, UQ(field.name) == field.value)
+      }
+    }
+  }
+  return (nrow(datos))
+}
+Unir      <- function(itemset.1, itemset.2) {
+  col.names <- union(names(itemset.1), names(itemset.2))
+  itemset   <- list()
+  for (col.name in col.names) {
+    col.values.1        <- itemset.1[[col.name]]
+    col.values.2        <- itemset.2[[col.name]]
+    itemset[[col.name]] <- union(col.values.1, col.values.2)
+  }
+  return (itemset)
+}
+Soporte   <- function(datos, itemset) {
+  return (Contar(datos, itemset) / nrow(datos))
+}
+Confianza <- function(datos, itemset.x, itemset.y) {
+  itemset.z <- Unir(itemset.x, itemset.y)
+  return (Contar(datos, itemset.z) / Contar(datos, itemset.x))
+}
+
+Soporte(titanic.raw, list(Survived = "Yes"))
+Soporte(titanic.raw, list(Survived = "Yes", Sex = "Male"))
+Soporte(titanic.raw, list(Survived = "Yes", Sex = "Male", Class = "3rd"))
+Soporte(titanic.raw, list(Survived = "No", Sex = "Male", Class = "3rd"))
+
+Confianza(titanic.raw, list(Class = "Crew"), list(Survived = "Yes"))
+Confianza(titanic.raw, list(Class = "1st"), list(Survived = "Yes"))
+Confianza(titanic.raw, list(Class = "Crew"), list(Survived = "No"))
+Confianza(titanic.raw, list(Class = "1st"), list(Survived = "No"))
+Confianza(titanic.raw, list(Age = "Adult", Sex = "Female"), list(Survived = "Yes"))
+Confianza(titanic.raw, list(Age = "Adult", Sex = "Male"), list(Survived = "No"))
