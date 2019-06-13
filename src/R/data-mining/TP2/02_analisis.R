a914e9b9233a7b6381ed05744ee5a27604d88c8a
Santiago Rovere <srovere@gmail.com>
2019-06-13 09:43:00 -0300

    Inicio de script de analisis

---
+++ src/R/data-mining/TP2/02_analisis.R
@@ -0,0 +1,24 @@
+# ---------------------------------------------------------------------------------------#
+# ---- Script para análisis de set de datos para TP2 ----                            
+# ---------------------------------------------------------------------------------------#
+
+# ---------------------------------------------------------------------------------------#
+# ---- I. Inicialización de ambiente ----                            
+# ---------------------------------------------------------------------------------------#
+rm(list = objects())
+
+require(dplyr)
+require(ggplot2)
+require(ggrepel)
+require(gridExtra)
+require(magrittr)
+require(purrr)
+require(scatterpie)
+require(stringr)
+require(sf)
+require(tidyr)
+require(tm)
+
+load(file = paste0(getwd(), "/input/PreciosClaros.RData"))
+load(file = paste0(getwd(), "/input/ReglasAsociacion.RData"))
+# ----------------------------------------------------------------------------------------
