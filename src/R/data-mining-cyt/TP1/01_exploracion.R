cb42d277cbb48babb4ca32f9deea98712083ad5f
Santiago Rovere <srovere@gmail.com>
2019-09-23 09:24:25 -0300

    Analisis exploratorio de set de datos

--- src/R/data-mining-cyt/TP1/01_exploracion.R
+++ src/R/data-mining-cyt/TP1/01_exploracion.R
@@ -11,8 +11,10 @@ require(dplyr)
 require(ggplot2)
 require(ggbiplot)
 require(GGally)
+require(jsonlite)
 require(magrittr)
 require(purrr)
+require(randomForest)
 require(readr)
 require(tidyr)
 # ----------------------------------------------------------------------------------------
@@ -21,16 +23,33 @@ require(tidyr)
 # ---- II. Lectura del set de datos  ----                            
 # ---------------------------------------------------------------------------------------#
 
-audio.features <- readr::read_csv(file = "input/audio_features.csv") 
-rownames(audio.features) <- audio.features$id
-audio.features %<>% dplyr::select(-id)
+# i. Metadatos
+metadatos <- readr::read_csv(file = "input/metadata.csv")
+
+# ii. Datos
+audio.features               <- readr::read_csv(file = "input/audio_features.csv") %>%
+  as.data.frame()
+
+# iii. Datos para PCA
+pca.audio.features           <- audio.features
+rownames(pca.audio.features) <- pca.audio.features$id
+pca.audio.features %<>% dplyr::select(-id)
+
+# iv. Datos para analisis de importancia de features para clasificar por genero
+clase <- "genre"
+audio.features.clase <- audio.features %>%
+  dplyr::inner_join(dplyr::select(metadatos, id, !! clase), by = "id") %>%
+  dplyr::select(-id) %>%
+  dplyr::mutate(clase = as.factor(!! rlang::sym(clase))) %>%
+  dplyr::select(- !! clase)
 # ----------------------------------------------------------------------------------------
 
 # ---------------------------------------------------------------------------------------#
 # ---- III. Exploracion de features  ----                            
 # ---------------------------------------------------------------------------------------#
 
-GGally::ggpairs(data = audio.features, columns = 1:ncol(audio.features)) +
+# Matriz de correlaciones y distribuciones
+GGally::ggpairs(data = pca.audio.features, columns = 1:ncol(pca.audio.features)) +
   ggplot2::labs(title = "Features de audio",
                 x = "", y = "") +
   ggplot2::theme_bw() +
@@ -40,9 +59,9 @@ GGally::ggpairs(data = audio.features, columns = 1:ncol(audio.features)) +
     plot.subtitle = ggplot2::element_text(hjust = 0.5)
   )
 
-pca            <- stats::princomp(x = audio.features, cor = TRUE)
+# PCA
+pca            <- stats::princomp(x = pca.audio.features, cor = TRUE)
 prop.varianzas <- (pca$sdev ^ 2) / sum((pca$sdev ^ 2))
-
 ggplot2::ggplot(data = data.frame(proporcion = 100 * cumsum(prop.varianzas), componente = 1:length(prop.varianzas)),
        mapping = ggplot2::aes(x = componente, y = proporcion, fill = componente)) +
   ggplot2::scale_x_continuous(breaks = 1:length(prop.varianzas)) +
@@ -59,4 +78,8 @@ ggbiplot::ggbiplot(pcobj = pca, ellipse = TRUE) +
   ggplot2::theme_bw() +
   ggplot2::theme(legend.position = 'right') +
   ggplot2::coord_fixed(ratio = 0.5)
+
+# Random Forest para buscar importancia de atributos
+modelo.rf <- randomForest::randomForest(clase ~ ., data = audio.features.clase)
+varImpPlot(modelo.rf)
 # ----------------------------------------------------------------------------------------
