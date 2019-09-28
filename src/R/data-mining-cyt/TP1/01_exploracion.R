0933d5dd6a0b49ff3218307bafa06608dfb26824
Santiago Rovere <srovere@gmail.com>
2019-09-28 14:34:54 -0300

    Mejora de graficos

--- src/R/data-mining-cyt/TP1/01_exploracion.R
+++ src/R/data-mining-cyt/TP1/01_exploracion.R
@@ -7,6 +7,7 @@
 # ---------------------------------------------------------------------------------------#
 rm(list = objects())
 
+require(Cairo)
 require(dplyr)
 require(ggplot2)
 require(ggbiplot)
@@ -17,6 +18,8 @@ require(purrr)
 require(randomForest)
 require(readr)
 require(tidyr)
+
+options(bitmapType = "cairo")
 # ----------------------------------------------------------------------------------------
 
 # ---------------------------------------------------------------------------------------#
@@ -49,9 +52,12 @@ audio.features.clase <- audio.features %>%
 # ---------------------------------------------------------------------------------------#
 
 # Matriz de correlaciones y distribuciones
-GGally::ggpairs(data = pca.audio.features, columns = 1:ncol(pca.audio.features)) +
-  ggplot2::labs(title = "Features de audio",
-                x = "", y = "") +
+GGally::ggpairs(data = pca.audio.features, 
+                columns = 1:ncol(pca.audio.features), 
+                #upper = list(continuous = wrap("density", alpha = 0.5), combo = "box"),
+                lower = list(continuous = wrap("points", alpha = 0.3,    size=0.1), 
+                             combo = wrap("dot", alpha = 0.4,            size=0.2) )) +
+  ggplot2::labs(title = "Asociación de features tomados de a pares", x = "", y = "") +
   ggplot2::theme_bw() +
   ggplot2::theme(
     legend.position = "bottom",
@@ -62,21 +68,30 @@ GGally::ggpairs(data = pca.audio.features, columns = 1:ncol(pca.audio.features))
 # PCA
 pca            <- stats::princomp(x = pca.audio.features, cor = TRUE)
 prop.varianzas <- (pca$sdev ^ 2) / sum((pca$sdev ^ 2))
-ggplot2::ggplot(data = data.frame(proporcion = 100 * cumsum(prop.varianzas), componente = 1:length(prop.varianzas)),
+ggplot2::ggplot(data = data.frame(proporcion = 100 * prop.varianzas, componente = 1:length(prop.varianzas)),
        mapping = ggplot2::aes(x = componente, y = proporcion, fill = componente)) +
   ggplot2::scale_x_continuous(breaks = 1:length(prop.varianzas)) +
   ggplot2::geom_bar(stat = 'identity') +
   ggplot2::geom_text(mapping = ggplot2::aes(y = proporcion + 1, label = sprintf("%0.2f", proporcion))) +
-  ggplot2::labs(x = "Componente", y = "Porcentaje acumulado de varianza explicada") +
+  ggplot2::labs(x = "Componente", y = "Porcentaje", title = "Porcentaje de varianza explicada por cada componente") +
   ggplot2::theme_bw() +
-  ggplot2::theme(legend.position = 'none')
+  ggplot2::theme(
+    legend.position = "none",
+    plot.title = ggplot2::element_text(hjust = 0.5),
+    plot.subtitle = ggplot2::element_text(hjust = 0.5)
+  )
 
-ggbiplot::ggbiplot(pcobj = pca, ellipse = TRUE) +
+ggbiplot::ggbiplot(pcobj = pca, ellipse = TRUE, alpha = 0) +
   ggplot2::scale_colour_brewer(name = 'Región', type = "qual", palette = "Set1") +
   ggplot2::labs(x = sprintf("Componente 1 (%0.2f%%)", 100*prop.varianzas[1]),
-                y = sprintf("Componente 2 (%0.2f%%)", 100*prop.varianzas[2])) +
+                y = sprintf("Componente 2 (%0.2f%%)", 100*prop.varianzas[2]),
+                title = "Biplot de las primeras 2 componentes") +
   ggplot2::theme_bw() +
-  ggplot2::theme(legend.position = 'right') +
+  ggplot2::theme(
+    legend.position = "bottom",
+    plot.title = ggplot2::element_text(hjust = 0.5),
+    plot.subtitle = ggplot2::element_text(hjust = 0.5)
+  )
   ggplot2::coord_fixed(ratio = 0.5)
 
 # Random Forest para buscar importancia de atributos
