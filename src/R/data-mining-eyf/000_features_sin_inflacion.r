512a01a3b634ecf4533dc49c66d2a61343a06d51
Santiago Rovere <srovere@gmail.com>
2019-10-13 19:42:00 +0000

    Uso de GA en pruebas con 3 meses

--- src/R/data-mining-eyf/000_features_sin_inflacion.r
+++ src/R/data-mining-eyf/000_features_sin_inflacion.r
@@ -69,34 +69,58 @@ test <- leer_set_datos_mensuales(paste0(config$dir$input, "/months"),
 # -----------------------------------------------------------------------------#
 
 # --- XGBoost - una sola corrida
-set.seed(config$semillas[1])
-xgb.train <- xgboost::xgb.DMatrix(data = as.matrix(dplyr::select(train, -clase)),
-                                  label = as.matrix(dplyr::select(train, clase)))
-xgb.test  <- xgboost::xgb.DMatrix(data = as.matrix(dplyr::select(test, -clase)),
-                                  label = as.matrix(dplyr::select(test, clase)))
+# set.seed(config$semillas[1])
+# xgb.train <- xgboost::xgb.DMatrix(data = as.matrix(dplyr::select(train, -clase)),
+#                                   label = as.matrix(dplyr::select(train, clase)))
+# xgb.test  <- xgboost::xgb.DMatrix(data = as.matrix(dplyr::select(test, -clase)),
+#                                   label = as.matrix(dplyr::select(test, clase)))
+# 
+# parametros <- list(
+#   booster = "gbtree",
+#   objective = "binary:logistic",
+#   groy_policy = "lossguide",
+#   tree_method = 'hist',
+#   eta = 0.009235036,
+#   max_depth = 14,
+#   gamma = 2.157951,
+#   subsample =  0.7654432,
+#   colsample_bytree = 0.7868235,
+#   min_child_weight = 4.528278 
+# )
+# 
+# set.seed(config$semillas[1])
+# start.time     <- proc.time()
+# modelo         <- xgboost::xgb.train(data = xgb.train, nrounds = 500, verbose = 2, maximize = FALSE,
+#                                      feval = pe_perdida_xgboost, 
+#                                      watchlist = list(train = xgb.train, test = xgb.test),
+#                                      params = parametros)
+# xgb.pred.test  <- data.frame(pred = predict(modelo, xgb.test, reshape = T))
+# pe_ganancia(probabilidades = xgb.pred.test$pred, clase = test$clase, proporcion = 1)
+# end.time       <- proc.time()
+# elapsed.time   <- end.time[3] - start.time[3]
+# logger$info(paste0("Tiempo:", elapsed.time, "segundos"))
 
-parametros <- list(
-  booster = "gbtree",
-  objective = "binary:logistic",
-  groy_policy = "lossguide",
-  tree_method = 'hist',
-  eta = 0.009235036,
-  max_depth = 14,
-  gamma = 2.157951,
-  subsample =  0.7654432,
-  colsample_bytree = 0.7868235,
-  min_child_weight = 4.528278 
+# --- XGBoost con optimizacion basada en algoritmos geneticos
+start.time         <- proc.time()
+limites.parametros <- list(
+  eta = c(0.001, 0.01),
+  max_depth = c(10, 20),
+  gamma = c(1, 5),
+  subsample = c(0.5, 1),
+  colsample_bytree = c(0.5, 1),
+  min_child_weight = c(1, 10),
+  scale_pos_weight = c(1, 100)
 )
 
-set.seed(config$semillas[1])
-start.time     <- proc.time()
-modelo         <- xgboost::xgb.train(data = xgb.train, nrounds = 500, verbose = 2, maximize = FALSE,
-                                     feval = pe_perdida_xgboost, 
-                                     watchlist = list(train = xgb.train, test = xgb.test),
-                                     params = parametros)
-xgb.pred.test  <- data.frame(pred = predict(modelo, xgb.test, reshape = T))
-pe_ganancia(probabilidades = xgb.pred.test$pred, clase = test$clase, proporcion = 1)
-end.time       <- proc.time()
-elapsed.time   <- end.time[3] - start.time[3]
+funcion_modelo    <- m_xgboost_closure(booster = "gbtree", objective = "binary:logistic", eval_metric = pe_perdida_xgboost, 
+                                       tree_method = "hist", grow_policy = "lossguide", nrounds = 500)
+resultados.xgb.ga <- ps_ga_optimization(set.datos = train, set.datos.test = test, clase = "clase", semillas = config$semillas, 
+                                        proporcion_train = 0.7, funcion_modelo = funcion_modelo, 
+                                        max_iterations = 10, tamano_poblacion = 20, run = 50,
+                                        funcion_prediccion = pr_xgboost, limites.parametros = limites.parametros, 
+                                        logger = logger)
+end.time          <- proc.time()
+elapsed.time      <- end.time[3] - start.time[3]
 logger$info(paste0("Tiempo:", elapsed.time, "segundos"))
+save(resultados.xgb.ga, file = "/home/santiago/xgboost.ga.RData")
 # ------------------------------------------------------------------------------
