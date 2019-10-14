b3f9c11b130b15dbae158b6e7f17354bc8543dcd
Santiago Rovere <srovere@gmail.com>
2019-10-14 17:08:40 -0300

    Primer borrador de clustering jerarquico

--- src/R/data-mining-cyt/TP1.r
+++ src/R/data-mining-cyt/TP1.r
@@ -8,14 +8,14 @@
 rm(list = objects())
 
 require(Cairo)
+require(caret)
+require(cluster)
 require(dplyr)
 require(ggplot2)
 require(ggbiplot)
 require(GGally)
-require(jsonlite)
-require(magrittr)
+require(MASS)
 require(purrr)
-require(randomForest)
 require(readr)
 require(tidyr)
 
@@ -23,49 +23,264 @@ options(bitmapType = "cairo")
 # ----------------------------------------------------------------------------------------
 
 # ---------------------------------------------------------------------------------------#
-# ---- II. Lectura del set de datos  ----                            
+# ---- II. Lectura de metadatos y set de audio_analisis  ----                            
 # ---------------------------------------------------------------------------------------#
 
 # i. Metadatos
 metadatos <- readr::read_csv(file = "input/metadata.csv")
 
-# ii. Datos
-audio.features               <- readr::read_csv(file = "input/audio_features.csv") %>%
-  as.data.frame()
+# ii. Datos de audio_features
+audio.analisis.rdata <- paste0(getwd(), "/input/audio_analysis.RData")
+if (! file.exists(audio.analisis.rdata)) {
+  audio.analysis <- purrr::map_dfr(
+    .x = dplyr::pull(audio.features, id),
+    .f = function(id) {
+      timbres.file  <- paste0(getwd(), "/input/audio_analysis/timbre/", id, ".csv")
+      pitches.file <- paste0(getwd(), "/input/audio_analysis/pitches/", id, ".csv")
+      
+      if (file.exists(timbres.file) && file.exists(pitches.file)) {
+        timbres <- readr::read_csv(file = timbres.file) %>%
+          dplyr::summarise_all(funs(mean)) %>%
+          dplyr::select(-start)
+        colnames(timbres) <- paste0("timbre_", colnames(timbres))
+        pitches <- readr::read_csv(file = pitches.file) %>%
+          dplyr::summarise_all(funs(mean)) %>%
+          dplyr::select(-start)
+        colnames(pitches) <- paste0("pitch_", colnames(pitches))
+        
+        return (dplyr::bind_cols(
+          data.frame(id = id), timbres, pitches
+        ))
+      } else {
+        return (NULL)
+      }
+    }
+  )
+  
+  save(audio.analysis, file = audio.analisis.rdata)
+} else {
+  load(audio.analisis.rdata)
+}
 
-# iii. Datos para PCA
-pca.audio.features           <- audio.features
-rownames(pca.audio.features) <- pca.audio.features$id
-pca.audio.features %<>% dplyr::select(-id)
+# iii. Escalar los datos
+audio.analysis <- audio.analysis %>%
+  dplyr::mutate_if(is.numeric, scale)
 
-# iv. Datos para analisis de importancia de features para clasificar por genero
-clase <- "genre"
-audio.features.clase <- audio.features %>%
-  dplyr::inner_join(dplyr::select(metadatos, id, !! clase), by = "id") %>%
+# iv. Pasaje a matriz
+m.audio.analysis           <- audio.analysis
+rownames(m.audio.analysis) <- m.audio.analysis$id
+m.audio.analysis           <- m.audio.analysis %>%
   dplyr::select(-id) %>%
+  as.matrix()
+
+# v. Datos para validacion externa con atributo "genero"
+clase <- "genre"
+audio.analysis.clase <- audio.analysis %>%
+  dplyr::inner_join(dplyr::select(metadatos, id, !! clase), by = "id") %>%
   dplyr::mutate(clase = as.factor(!! rlang::sym(clase))) %>%
   dplyr::select(- !! clase)
 # ----------------------------------------------------------------------------------------
 
 # ---------------------------------------------------------------------------------------#
-# ---- III. Exploracion de features  ----                            
+# ---- III. Lectura del set de audio_features y combinar  ----                            
 # ---------------------------------------------------------------------------------------#
 
-# Matriz de correlaciones y distribuciones
-GGally::ggpairs(data = pca.audio.features, 
-                columns = 1:ncol(pca.audio.features), 
-                #upper = list(continuous = wrap("density", alpha = 0.5), combo = "box"),
-                lower = list(continuous = wrap("points", alpha = 0.3,    size=0.1), 
-                             combo = wrap("dot", alpha = 0.4,            size=0.2) )) +
-  ggplot2::labs(title = "Asociación de features tomados de a pares", x = "", y = "") +
-  ggplot2::theme_bw() +
-  ggplot2::theme(
-    legend.position = "bottom",
-    plot.title = ggplot2::element_text(hjust = 0.5),
-    plot.subtitle = ggplot2::element_text(hjust = 0.5)
-  )
+# i. Lectura y filtrado
+audio.features               <- readr::read_csv(file = "input/audio_features.csv") %>%
+  dplyr::select(id, acousticness, danceability, energy, instrumentalness, liveness, 
+                loudness, speechiness, tempo, valence) %>%
+  as.data.frame() %>%
+  dplyr::filter(id %in% dplyr::pull(audio.analysis, id))
+
+# ii. Escalar los datos
+audio.analysis <- audio.analysis %>%
+  dplyr::mutate_if(is.numeric, scale)
+
+# iii. Pasaje a matriz
+m.audio.features           <- audio.features
+rownames(m.audio.features) <- m.audio.features$id
+m.audio.features           <- m.audio.features %>%
+  dplyr::select(-id) %>%
+  as.matrix()
+
+# iv. Datos para validacion externa con atributo "genero"
+clase <- "genre"
+audio.features.clase <- audio.features %>%
+  dplyr::inner_join(dplyr::select(metadatos, id, !! clase), by = "id") %>%
+  dplyr::mutate(clase = as.factor(!! rlang::sym(clase))) %>%
+  dplyr::select(- !! clase)
+
+# v. Combinar atributos
+audio.combinado <- audio.features %>%
+  dplyr::inner_join(audio.analysis)
+audio.combinado.clase <- audio.features.clase %>%
+  dplyr::inner_join(audio.analysis.clase)
+m.audio.combinado <- audio.combinado
+rownames(m.audio.combinado) <- m.audio.combinado$id
+m.audio.combinado <- m.audio.combinado %>%
+  dplyr::select(-id) %>%
+  as.matrix()
+# ----------------------------------------------------------------------------------------
+
+# ---------------------------------------------------------------------------------------#
+# ---- A1. Exploracion de hiperparámetros audio_features ----                            
+# ---------------------------------------------------------------------------------------#
+
+# Calculamos la matriz de distancia entre individuos (euclídea)
+matriz.distancia <- dist(x = m.audio.features, method = "euclidean")
+
+# Clusters con metodos de calculo de distancia entre clusters
+hc_complete <- hclust(d = matriz.distancia, method = "complete")
+hc_average  <- hclust(d = matriz.distancia, method = "average")
+hc_single   <- hclust(d = matriz.distancia, method = "single")
+hc_ward.D2  <- hclust(d = matriz.distancia, method = "ward.D2")
+hc_ward.D   <- hclust(d = matriz.distancia, method = "ward.D")
+hc_centroid <- hclust(d = matriz.distancia, method = "centroid")
+
+# Calculamos los coeficientes de correlación cofenética
+coeficientes.correlacion.cofenetica <- c(
+  "complete" = cor(x = matriz.distancia, cophenetic(hc_complete)),
+  "average" = cor(x = matriz.distancia, cophenetic(hc_average)),
+  "single" = cor(x = matriz.distancia, cophenetic(hc_single)),
+  "ward.D" = cor(x = matriz.distancia, cophenetic(hc_ward.D2)),
+  "ward.D2" = cor(x = matriz.distancia, cophenetic(hc_ward.D)),
+  "centroid" = cor(x = matriz.distancia, cophenetic(hc_centroid))
+)
+print(coeficientes.correlacion.cofenetica)
+
+# Usamos Complete Linkage
+cluster.jerarquico <- stats::hclust(d = matriz.distancia, method = "complete")
+n.clusters         <- seq(from = 2, to = 20)
+medias.silhouette  <- purrr::map_dfr(
+  .x = n.clusters,
+  .f = function(k) {
+    silhouette.k    <- silhouette(cutree(cluster.jerarquico, k), matriz.distancia)
+    return (data.frame(k = k, silhouette = mean(silhouette.k[, "sil_width"])))
+  }
+)
+
+# Graficamos el valor del índice de Silhouette para cada valor de k
+ggplot2::ggplot(data = medias.silhouette) +
+  ggplot2::geom_line(mapping = ggplot2::aes(x = k, y = silhouette))
+
+# Guardar variables del clustering
+cluster.jerarquico.features <- cluster.jerarquico
+matriz.distancia.features   <- matriz.distancia
+# ----------------------------------------------------------------------------------------
+
+# ---------------------------------------------------------------------------------------#
+# ---- A2. Exploracion de hiperparámetros audio_analysis ----                            
+# ---------------------------------------------------------------------------------------#
+
+# Calculamos la matriz de distancia entre individuos (euclídea)
+matriz.distancia <- dist(x = m.audio.analysis, method = "euclidean")
+
+# Clusters con metodos de calculo de distancia entre clusters
+hc_complete <- hclust(d = matriz.distancia, method = "complete")
+hc_average  <- hclust(d = matriz.distancia, method = "average")
+hc_single   <- hclust(d = matriz.distancia, method = "single")
+hc_ward.D2  <- hclust(d = matriz.distancia, method = "ward.D2")
+hc_ward.D   <- hclust(d = matriz.distancia, method = "ward.D")
+hc_centroid <- hclust(d = matriz.distancia, method = "centroid")
+
+# Calculamos los coeficientes de correlación cofenética
+coeficientes.correlacion.cofenetica <- c(
+  "complete" = cor(x = matriz.distancia, cophenetic(hc_complete)),
+  "average" = cor(x = matriz.distancia, cophenetic(hc_average)),
+  "single" = cor(x = matriz.distancia, cophenetic(hc_single)),
+  "ward.D" = cor(x = matriz.distancia, cophenetic(hc_ward.D2)),
+  "ward.D2" = cor(x = matriz.distancia, cophenetic(hc_ward.D)),
+  "centroid" = cor(x = matriz.distancia, cophenetic(hc_centroid))
+)
+print(coeficientes.correlacion.cofenetica)
+
+# Usamos Complete Linkage
+cluster.jerarquico <- stats::hclust(d = matriz.distancia, method = "average")
+n.clusters         <- seq(from = 2, to = 20)
+medias.silhouette  <- purrr::map_dfr(
+  .x = n.clusters,
+  .f = function(k) {
+    silhouette.k    <- silhouette(cutree(cluster.jerarquico, k), matriz.distancia)
+    return (data.frame(k = k, silhouette = mean(silhouette.k[, "sil_width"])))
+  }
+)
+
+# Graficamos el valor del índice de Silhouette para cada valor de k
+ggplot2::ggplot(data = medias.silhouette) +
+  ggplot2::geom_line(mapping = ggplot2::aes(x = k, y = silhouette))
+
+# Guardar variables del clustering
+cluster.jerarquico.analysis <- cluster.jerarquico
+matriz.distancia.analysis   <- matriz.distancia
+# ----------------------------------------------------------------------------------------
+
+# ---------------------------------------------------------------------------------------#
+# ---- A3. Exploracion de hiperparámetros audio_analysis + audio_features ----                            
+# ---------------------------------------------------------------------------------------#
+
+# Calculamos la matriz de distancia entre individuos (euclídea)
+matriz.distancia <- dist(x = m.audio.combinado, method = "euclidean")
+
+# Clusters con metodos de calculo de distancia entre clusters
+hc_complete <- hclust(d = matriz.distancia, method = "complete")
+hc_average  <- hclust(d = matriz.distancia, method = "average")
+hc_single   <- hclust(d = matriz.distancia, method = "single")
+hc_ward.D2  <- hclust(d = matriz.distancia, method = "ward.D2")
+hc_ward.D   <- hclust(d = matriz.distancia, method = "ward.D")
+hc_centroid <- hclust(d = matriz.distancia, method = "centroid")
+
+# Calculamos los coeficientes de correlación cofenética
+coeficientes.correlacion.cofenetica <- c(
+  "complete" = cor(x = matriz.distancia, cophenetic(hc_complete)),
+  "average" = cor(x = matriz.distancia, cophenetic(hc_average)),
+  "single" = cor(x = matriz.distancia, cophenetic(hc_single)),
+  "ward.D" = cor(x = matriz.distancia, cophenetic(hc_ward.D2)),
+  "ward.D2" = cor(x = matriz.distancia, cophenetic(hc_ward.D)),
+  "centroid" = cor(x = matriz.distancia, cophenetic(hc_centroid))
+)
+print(coeficientes.correlacion.cofenetica)
+
+# Usamos Complete Linkage
+cluster.jerarquico <- stats::hclust(d = matriz.distancia, method = "average")
+n.clusters         <- seq(from = 2, to = 20)
+medias.silhouette  <- purrr::map_dfr(
+  .x = n.clusters,
+  .f = function(k) {
+    silhouette.k    <- silhouette(cutree(cluster.jerarquico, k), matriz.distancia)
+    return (data.frame(k = k, silhouette = mean(silhouette.k[, "sil_width"])))
+  }
+)
+
+# Graficamos el valor del índice de Silhouette para cada valor de k
+ggplot2::ggplot(data = medias.silhouette) +
+  ggplot2::geom_line(mapping = ggplot2::aes(x = k, y = silhouette))
+
+# Guardar variables del clustering
+cluster.jerarquico.combinado <- cluster.jerarquico
+matriz.distancia.combinado   <- matriz.distancia
+# ----------------------------------------------------------------------------------------
+
+# ---------------------------------------------------------------------------------------#
+# ---- B. Evaluar agrupamientos ----                            
+# ---------------------------------------------------------------------------------------#
+
+# Generamos clusterizacion para k = 5 (dado que esto va a servir para comparar con los generos)
+grupos.jerarquico <- audio.combinado.clase %>%
+  dplyr::select(id, clase) %>%
+  dplyr::mutate(clase_features = stats::cutree(cluster.jerarquico.features, k = 5),
+                clase_analysis = stats::cutree(cluster.jerarquico.analysis, k = 5),
+                clase_combinado = stats::cutree(cluster.jerarquico.combinado, k = 5))
+
+table(grupos.jerarquico$clase_analysis, grupos.jerarquico$clase_analysis)
+table(grupos.jerarquico$clase_analysis, grupos.jerarquico$clase_combinado)
+table(grupos.jerarquico$clase_features, grupos.jerarquico$clase_combinado)
+
 
 # PCA
+pca.audio.features           <- audio.features
+rownames(pca.audio.features) <- pca.audio.features$id
+pca.audio.features %<>% dplyr::select(-id)
+
 pca            <- stats::princomp(x = pca.audio.features, cor = TRUE)
 prop.varianzas <- (pca$sdev ^ 2) / sum((pca$sdev ^ 2))
 ggplot2::ggplot(data = data.frame(proporcion = 100 * prop.varianzas, componente = 1:length(prop.varianzas)),
@@ -93,8 +308,3 @@ ggbiplot::ggbiplot(pcobj = pca, ellipse = TRUE, alpha = 0) +
     plot.subtitle = ggplot2::element_text(hjust = 0.5)
   )
   ggplot2::coord_fixed(ratio = 0.5)
-
-# Random Forest para buscar importancia de atributos
-modelo.rf <- randomForest::randomForest(clase ~ ., data = audio.features.clase)
-varImpPlot(modelo.rf)
-# ----------------------------------------------------------------------------------------
