6da2cbc87112ecc7791ae37917c837ac8a8588b5
Santiago Rovere <srovere@gmail.com>
2019-11-12 08:57:06 -0300

    Eliminacion de paquetes innecesarios

--- src/R/data-mining-eyf/001_hiperparametrizacion.r
+++ src/R/data-mining-eyf/001_hiperparametrizacion.r
@@ -3,9 +3,8 @@
 # -----------------------------------------------------------------------------#
 rm(list = ls()); gc()
 Sys.setenv(TZ = "UTC")
-list.of.packages <- c("caret", "data.table", "doSNOW", "dplyr", "foreach", 
-                      "futile.logger", "GA", "parallel", "R6", "mlrMBO", "rgenoud",
-                      "ROCR", "rpart", "snow", "utils", "xgboost", "yaml")
+list.of.packages <- c("caret", "data.table", "dplyr", "futile.logger", "R6", 
+                      "mlrMBO", "rgenoud", "ROCR", "utils", "xgboost", "yaml")
 for (pack in list.of.packages) {
   if (! require(pack, character.only = TRUE)) {
     stop(paste0("Paquete no encontrado: ", pack))
