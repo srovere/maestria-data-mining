037bbea2a52cf183baa77814e5061d5861406cea
Santiago Rovere <srovere@gmail.com>
2019-12-12 16:14:19 -0300

    Correcciones varias

--- src/R/data-mining-cyt/TP2.r
+++ src/R/data-mining-cyt/TP2.r
@@ -11,8 +11,10 @@ require(Cairo)
 require(caret)
 require(dplyr)
 require(ggplot2)
+require(gtools)
 require(igraph)
 require(magrittr)
+require(mclust)
 require(purrr)
 require(readr)
 require(stringr)
@@ -91,7 +93,6 @@ ObtenerGrafosDensidades <- function(matriz.correlacion, estadio, sujeto, densida
   )
 }
 
-
 if (file.exists("output/Grafos.RData")) {
   load(file = "output/Grafos.RData")
 } else {
@@ -109,9 +110,37 @@ if (file.exists("output/Grafos.RData")) {
       return (ObtenerGrafosDensidades(matriz.correlacion, estadio, sujeto, densidades))
     }
   )
+  
+  # Grafos promedio para ciertas densidades (0.075, 0.25 y 0.5)
+  grafos.promedio <- purrr::pmap_dfr(
+    .l = dplyr::distinct(datos.entrada, estadio),
+    .f = function(estadio) {
+      sujetos <- datos.entrada %>%
+        dplyr::filter(estadio == !! estadio) %>%
+        dplyr::distinct(sujeto) %>%
+        dplyr::pull(sujeto)
+      matriz.promedio <- NULL
+      for (sujeto in sujetos) {
+        matriz.correlacion <- datos.entrada %>%
+          dplyr::filter(estadio == !! estadio & sujeto == !! sujeto) %>%
+          dplyr::select(-estadio, -sujeto) %>%
+          as.matrix()
+        if (! is.null(matriz.promedio)) {
+          matriz.promedio <- matriz.promedio + matriz.correlacion
+        } else {
+          matriz.promedio <- matriz.correlacion
+        }
+      }
+      matriz.promedio <- matriz.promedio / length(sujetos)
+      
+      # Generar grafos para distintas densidades
+      densidades <- c(0.075, 0.25, 0.5)
+      return (ObtenerGrafosDensidades(matriz.promedio, estadio, NA, densidades))
+    }
+  ) %>% dplyr::select(-sujeto)
 
   # Guardar grafos a un archivo RData
-  save(grafos, file = "output/Grafos.RData")
+  save(grafos, grafos.promedio, file = "output/Grafos.RData")
 }
 # ----------------------------------------------------------------------------------------
 
@@ -166,7 +195,7 @@ etiquetas_metricas <- c(
   coeficiente_clustering = "Coeficiente de clustering"
 )
 
-# Generar grafico
+# Generar grafico de metricas
 ggplot2::ggplot(data = estadisticas) +
   ggplot2::geom_errorbar(mapping = ggplot2::aes(x = densidad, ymin = media - desvio, ymax = media + desvio, col = estadio)) +
   ggplot2::facet_wrap(~metrica, scales = "free", ncol = 2, labeller = ggplot2::labeller(metrica = etiquetas_metricas)) +
@@ -179,6 +208,8 @@ ggplot2::ggplot(data = estadisticas) +
     plot.title = ggplot2::element_text(hjust = 0.5),
     plot.subtitle = ggplot2::element_text(hjust = 0.5)
   )
+
+# TODO: Mostrar algunos grafos promedio
 # ----------------------------------------------------------------------------------------
 
 # ---------------------------------------------------------------------------------------#
@@ -212,13 +243,24 @@ if (file.exists("output/MetricasLouvain.RData")) {
       numero.com.random  <- length(unique(comunidad.random$membership))
       
       return (dplyr::bind_rows(
-        data.frame(estadio = estadio, sujeto = sujeto, grafo = 'Original', densidad = densidad,
-                   modularidad = modularidad.original, numero_comunidades = numero.com.original),
-        data.frame(estadio = estadio, sujeto = sujeto, grafo = 'Random', densidad = densidad,
-                   modularidad = modularidad.random, numero_comunidades = numero.com.random)
+        dplyr::tibble(estadio = estadio, sujeto = sujeto, grafo = 'Original', densidad = densidad,
+                      modularidad = modularidad.original, numero_comunidades = numero.com.original, 
+                      membresias = list(comunidad.original$membership)),
+        dplyr::tibble(estadio = estadio, sujeto = sujeto, grafo = 'Random', densidad = densidad,
+                      modularidad = modularidad.random, numero_comunidades = numero.com.random, membrerias = NA)
       )) 
     }
-  ) %>% tidyr::pivot_longer(names_to = "metrica", values_to = "valor", cols = c("modularidad", "numero_comunidades"))
+  )
+  
+  # Obtener membresidas
+  membresias <- metricas.louvain %>%
+    dplyr::filter(grafo == "Original") %>%
+    dplyr::select(estadio, sujeto, densidad, membresias)
+  
+  # Quitar membrerias de metricas y pasar a data frame largo
+  metricas.louvain <- metricas.louvain %>% 
+    dplyr::select(-membresias) %>%
+    tidyr::pivot_longer(names_to = "metrica", values_to = "valor", cols = c("modularidad", "numero_comunidades"))
   
   # Calcular estadisticas
   estadisticas.louvain <- metricas.louvain %>%
@@ -227,7 +269,7 @@ if (file.exists("output/MetricasLouvain.RData")) {
                      desvio = sd(valor, na.rm = TRUE))
   
   # Guardar metricas y estadisticas a archivo RData
-  save(metricas.louvain, estadisticas.louvain, file = "output/MetricasLouvain.RData")
+  save(metricas.louvain, estadisticas.louvain, membresias, file = "output/MetricasLouvain.RData")
 }
   
 # Etiquetas para metricas
@@ -338,6 +380,7 @@ ggplot2::ggplot(data = estadisticas.louvain.clauset) +
 # ---- Tarea 3 ----                            
 # ---------------------------------------------------------------------------------------#
 
+# t-tests para modularidad
 modularidad <- metricas.louvain %>%
   dplyr::filter(grafo == "Original" & metrica == "modularidad") %>%
   dplyr::select(-grafo, -metrica) %>%
@@ -352,18 +395,20 @@ modularidad <- metricas.louvain %>%
   ) %>%
   dplyr::select(densidad, p_valor_N1, p_valor_N2, p_valor_N3)
 
-t.tests <-  metricas.louvain %>%
-  dplyr::filter(grafo == "Original") %>%
-  dplyr::select(-grafo, -sujeto) %>%
-  dplyr::group_by(densidad, estadio, metrica) %>%
-  dplyr::summarise(valores = list(valor)) %>%
-  tidyr::pivot_wider(names_from = "estadio", values_from = "valores") %>%
-  dplyr::mutate(
-    p_valor_N1 = t.test(x = unlist(N1), y = unlist(W), paired = FALSE)$p.value,
-    p_valor_N2 = t.test(x = unlist(N2), y = unlist(W), paired = FALSE)$p.value,
-    p_valor_N3 = t.test(x = unlist(N3), y = unlist(W), paired = FALSE)$p.value
+# t-tests para numero de comunidades
+numero.comunidades <- metricas.louvain %>%
+  dplyr::filter(grafo == "Original" & metrica == "numero_comunidades") %>%
+  dplyr::select(-grafo, -metrica) %>%
+  tidyr::pivot_wider(names_from = "estadio", values_from = "valor") %>%
+  dplyr::mutate(N1 = N1 - W, N2 = N2 - W, N3 = N3 - W) %>%
+  dplyr::select(-W) %>%
+  dplyr::group_by(densidad) %>%
+  dplyr::summarise(
+    p_valor_N1 = t.test(x = unlist(N1), alternative = "greater")$p.value,
+    p_valor_N2 = t.test(x = unlist(N2), alternative = "greater")$p.value,
+    p_valor_N3 = t.test(x = unlist(N3), alternative = "greater")$p.value
   ) %>%
-  dplyr::select(densidad, metrica, p_valor_N1, p_valor_N2, p_valor_N3)
+  dplyr::select(densidad, p_valor_N1, p_valor_N2, p_valor_N3)
 
 # Generar grafico comparativo
 datos.grafico.comparacion.estadios <- estadisticas.louvain %>%
@@ -413,6 +458,131 @@ ggplot2::ggplot() +
   ggplot2::labs(x = "Densidad de aristas", y = "Valor de la métrica", col = "Tipo de grafo",
                 title = "Métricas de modularidad para grafos según de densidad de corte",
                 subtitle = "Valores medios y desvíos") +
+  ggplot2::theme_bw() +
+  ggplot2::theme(
+    legend.position = 'bottom',
+    plot.title = ggplot2::element_text(hjust = 0.5),
+    plot.subtitle = ggplot2::element_text(hjust = 0.5),
+    axis.text.x = ggplot2::element_text(angle = 90)
+  )
+# ----------------------------------------------------------------------------------------
+
+# ---------------------------------------------------------------------------------------#
+# ---- Tarea 4 ----                            
+# ---------------------------------------------------------------------------------------#
+
+if (file.exists("output/IndicesRandMembresias.RData")) {
+  load(file = "output/IndicesRandMembresias.RData")
+} else {
+  # 1-2. Para todos los pares de sujetos de Nx se calcula el indice de Rand ajustado
+  #      de las comunidades. Esto debe hacerse para todas las densidades
+  indices.rand <- purrr::map_dfr(
+    .x = list('N1', 'N2', 'N3', 'W'),
+    .f = function(estadio) {
+      # Seleccionar los datos de membresia para ese estadio
+      membresias.estadio <- membresias %>%
+        dplyr::filter(estadio == !! estadio) %>%
+        dplyr::select(-estadio)
+        
+      # Obtener todas las combinaciones posibles de sujetos tomados de a 2
+      sujetos <- membresias.estadio %>%
+        dplyr::pull(sujeto) %>%
+        unique()
+      pares.sujetos <- gtools::combinations(n = length(sujetos), r = 2, v = sujetos) %>%
+        as.data.frame() %>%
+        dplyr::rename(sujeto_x = V1, sujeto_y = V2)
+      
+      # Obtener densidades para cada estadio
+      densidades <- membresias.estadio %>%
+        dplyr::distinct(densidad)
+      
+      # Cruzar las combinaciones de sujetos con las densidades
+      combinaciones <- tidyr::crossing(pares.sujetos, densidades)
+      
+      # Para cada densidad, calcular el indice Rand ajustado para las membresias
+      # de cada combinacion de sujetos
+      indices.rand.estadio <- combinaciones %>%
+        dplyr::inner_join(membresias.estadio, by = c("sujeto_x" = "sujeto", "densidad")) %>%
+        dplyr::rename(membresias_x = membresias) %>%
+        dplyr::inner_join(membresias.estadio, by = c("sujeto_y" = "sujeto", "densidad")) %>%
+        dplyr::rename(membresias_y = membresias) %>%
+        dplyr::mutate(rand_ajustado = purrr::map2(
+          .x = membresias_x,
+          .y = membresias_y,
+          .f = function(mx, my) {
+            return (mclust::adjustedRandIndex(unlist(mx), unlist(my)))  
+          }
+        ) %>% unlist()) %>%
+        dplyr::mutate(estadio = estadio) %>%
+        dplyr::select(estadio, densidad, sujeto_x, sujeto_y, rand_ajustado)
+    }
+  )
+  
+  # 3. Calcular promedio y desvio de los indices Rand para cada estadio y densidad
+  estadisticas.rand.observadas <- indices.rand %>%
+    dplyr::group_by(estadio, densidad) %>%
+    dplyr::summarise(media = mean(rand_ajustado, na.rm = TRUE),
+                     desvio = sd(rand_ajustado, na.rm = TRUE))
+  
+  # 4. Permutaciones: Se realizan N permutaciones entre los estadios Nx-W para calcular nuevamente las medias de
+  #    Rand ajustado.
+  set.seed(0)
+  permutaciones.estadios <- purrr::cross_df(
+    .l = list(
+      estadio = c('N1', 'N2', 'N3'), 
+      densidad = dplyr::distinct(membresias, densidad) %>% dplyr::pull(densidad)
+    )
+  )
+  estadisticas.rand.permutadas <- purrr::pmap_dfr(
+    .l = permutaciones.estadios,
+    .f = function(estadio, densidad) {
+      estadios <- c(estadio, 'W')
+      indices.rand.combinacion <- indices.rand %>%
+        dplyr::filter(estadio %in% estadios & densidad == !! densidad) %>%
+        dplyr::select(-densidad) %>%
+        tidyr::pivot_wider(id_cols = c("estadio", "sujeto_x", "sujeto_y"), names_from = "estadio", values_from = "rand_ajustado") %>%
+        dplyr::rename(N = !! estadio)
+      
+      # Generar N permutaciones distintas y calcular nuevamente el promedio para Nx y W
+      estadisticas.rand <- purrr::map_dfr(
+        .x = seq(from = 1, to = 1000),
+        .f = function(permutacion) {
+          indices.rand.permutacion <- indices.rand.combinacion %>%
+            dplyr::mutate(
+              permutar = as.logical(rbinom(n = nrow(indices.rand.combinacion), size = 1, prob = 0.5)),
+              nuevo_N := dplyr::if_else(permutar, W, N), 
+              nuevo_W := dplyr::if_else(permutar, N, W)
+            ) %>%
+            dplyr::select(-N, -W) %>%
+            dplyr::rename(N = nuevo_N, W = nuevo_W) %>%
+            dplyr::mutate(permutacion = permutacion)
+        }
+      ) %>% dplyr::group_by(permutacion) %>%
+        dplyr::summarise(media_nueva = mean(N, na.rm = TRUE)) %>%
+        dplyr::mutate(estadio = estadio, densidad = densidad)
+      
+      # Comparar y calcular p-valor
+      estadisticas.rand.observadas.combinacion <- estadisticas.rand.observadas %>%
+        dplyr::inner_join(estadisticas.rand, by = c("estadio", "densidad")) %>%
+        dplyr::mutate(mayor = dplyr::if_else(media_nueva > media, 1, 0))
+      return (estadisticas.rand.observadas.combinacion)
+    }
+  ) %>%
+    dplyr::filter(! is.na(mayor)) %>%
+    dplyr::group_by(estadio, densidad) %>%
+    dplyr::summarise(cantidad = sum(mayor), total = dplyr::n()) %>%
+    dplyr::mutate(p_valor = cantidad / total)
+  
+  # Guardar los datos calculados
+  save(indices.rand, estadisticas.rand.observadas, estadisticas.rand.permutadas, file = "output/IndicesRandMembresias.RData")
+}
+
+# Graficar valores observados
+ggplot2::ggplot(data = dplyr::filter(estadisticas.rand.observadas, estadio %in% c("N2", "W"))) +
+  ggplot2::geom_errorbar(mapping = ggplot2::aes(x = densidad, ymin = media - desvio, ymax = media + desvio, col = estadio)) +
+  ggplot2::labs(x = "Densidad de aristas", y = "Rand ajustado", col = "Estadío",
+                title = "Índices de rand ajustados para membresías de sujetos",
+                subtitle = "Valores medios y desvío para cada densidad y estadío de sueño") +
   ggplot2::theme_bw() +
   ggplot2::theme(
     legend.position = 'bottom',
