c8de93751d73737c97a04bf352cb9588c6160448
Santiago Rovere <srovere@gmail.com>
2020-07-17 10:04:33 -0300

    Agregado de NDWI y NDVI

--- src/R/sistemas-informacion-geografica/TP2.r
+++ src/R/sistemas-informacion-geografica/TP2.r
@@ -40,22 +40,6 @@ rm(archivo.config, args); gc()
 # Definir directorios de trabajo
 working.directory <- paste0(getwd(), "/tp-teledeteccion2020")
 
-# Inicializar conexion con Copernicus Open Access Hub
-tryCatch({
-  # Primero se hace login en la ubicacion default (sino s2_download no funciona)
-  sen2r::write_scihub_login(username = config$credenciales$user,
-                            password = config$credenciales$pass)
-  
-  # Luego se hace lo mismo en un path especifico
-  sen2r::write_scihub_login(username = config$credenciales$user,
-                            password = config$credenciales$pass,
-                            apihub_path = paste0(working.directory, "/apihub.txt"))
-}, error = function(e) {
-  DBI::dbDisconnect(con)
-  script$stop(paste0("Error al realizar login: ", as.character(e)))  
-})
-
-
 # Los datos de ground truth están en EPSG:4326 o EPSG:22185 (POSGAR 94 / Argentina 5)
 # Definir area de interes
 area.of.interest <- geojsonsf::geojson_sf(paste0(working.directory, "/ground_truth/poligono_referencia.geojson")) %>%
@@ -63,8 +47,12 @@ area.of.interest <- geojsonsf::geojson_sf(paste0(working.directory, "/ground_tru
 
 # Cuerpos de agua estables
 water.bodies <- geojsonsf::geojson_sf(paste0(working.directory, "/ground_truth/aguas_continentales_a.geojson")) %>%
-  sf::st_transform(x = ., crs = 22185) %>%
+  sf::st_set_crs(x = ., value = 4326) %>%
+  sf::st_transform(x = ., 22185) %>%
   sf::st_intersection(area.of.interest)
+sf::st_write(obj = water.bodies, dsn = paste0(working.directory, "/ground_truth"), 
+             layer = "WaterBodies", driver = "ESRI shapefile", 
+             update = TRUE, delete_layer = TRUE)
 
 # Ground truth
 ground.truth <- geojsonsf::geojson_sf(paste0(working.directory, "/ground_truth/flood.geojson")) %>%
@@ -72,115 +60,41 @@ ground.truth <- geojsonsf::geojson_sf(paste0(working.directory, "/ground_truth/f
 # ------------------------------------------------------------------------------
 
 # -----------------------------------------------------------------------------#
-# --- PASO 4. Abrir datos de Enero de 2019  ----
+# --- PASO 4. Chuks de codigo para hacer distintas operaciones ----
 # -----------------------------------------------------------------------------#
 
 # Carpeta de imagenes satelitales
-images.directory <- paste0(working.directory, "/images")
+images.directory <- paste0(working.directory, "/images/masked")
 
 # Pasar area de interes a Lat-Lon para enmascarar imagenes
 # Enero de 2019
 area.of.interest.latlon <- sf::st_transform(area.of.interest, crs = 4326)
-s <- raster::stack(x = paste0(images.directory, "/merged/201811.tif")) %>%
+s <- raster::stack(x = paste0(images.directory, "/merged/201901.tif")) %>%
   raster::crop(x = ., y = raster::extent(area.of.interest.latlon)) %>%
   raster::mask(x = ., mask = area.of.interest.latlon)
-raster::writeRaster(x = s, filename = paste0(images.directory, "/masked/201811.tif"), format = "GTiff")
-raster::projectRaster(from = s, filename = paste0(images.directory, "/masked/201811_epsg_22185.tif"), 
+raster::writeRaster(x = s, filename = paste0(images.directory, "/masked/201901.tif"), format = "GTiff")
+raster::projectRaster(from = s, filename = paste0(images.directory, "/masked/201901_epsg_22185.tif"), 
                       crs = raster::crs(area.of.interest))
 
-  #raster::projectRaster(crs = sf::st_crs(area.of.interest)$proj4string)
-  #raster::crop(x = ., y = raster::ex)
-  
+# B2 (blue)
+# B3 (green)
+# B4 (red)
+# B8 (nir)
+datos.raster <- raster::stack(paste0(images.directory, "/201801_epsg_22185.tif"))
+green        <- datos.raster[[2]]
+red          <- datos.raster[[3]]
+nir          <- datos.raster[[4]]
 
+# Calculo de NDVI y NWDI
+ndwi         <- (green - nir) / (green + nir)
+ndvi         <- (nir - red) / (nir + red)
 
+# Agregar layers
+raster.con.indices        <- raster::addLayer(datos.raster, ndwi, ndvi)
+names(raster.con.indices) <- c("b2", "b3", "b4", "b8", "ndwi", "ndvi")
+
+# Guardar raster
+raster::writeRaster(x = raster.con.indices, 
+                    filename = paste0(working.directory, "/images/indices/201801.tif"), 
+                    format = "GTiff")
 # ------------------------------------------------------------------------------
-
-# -----------------------------------------------------------------------------#
-# --- PASO 4. Descargar datos de sentinel-2  ----
-# -----------------------------------------------------------------------------#
-
-# # Pasar area de interes a UTM 21S
-# area.of.interest.utm <- sf::st_transform(area.of.interest, crs = 32721)
-# 
-# # Obtener registros para descargar
-# registros <- sen2r::s2_list(spatial_extent = sf::st_geometry(sf::st_transform(x = area.of.interest, crs = config$proj4string$latlon)), 
-#                             time_interval = c(as.Date("2019-01-01"), as.Date("2019-01-31")),
-#                             level = config$producto$nivel.aceptacion,
-#                             max_cloud = 10,
-#                             apihub = paste0(working.directory, "/apihub.txt"))
-# 
-# # Determinar datos faltantes
-# output.directory <- paste0(working.directory, "/sentinel-2")
-# existentes       <- list.files(output.directory)
-# faltantes        <- registros[! (names(registros) %in% existentes)]
-# 
-# # Descargar datos faltantes
-# sen2r::s2_download(s2_prodlist = faltantes, 
-#                    apihub = paste0(working.directory, "/apihub.txt"),
-#                    outdir = output.directory)
-#   
-# # Preparar datos para ensamblarlos como rasters
-# translate.output <- purrr::map(
-#   .x = list.files(path = output.directory, pattern = "^S2", full.names = TRUE),
-#   .f = function(directorio) {
-#     sen2r::s2_translate(infile = directorio, outdir = output.directory, format = "GTiff", 
-#                         prod_type = c("BOA", "SCL"))
-#   }
-# ) %>% unlist()
-# 
-# # Hacer merge de los tiles si los hubiera
-# merge.output <- sen2r::s2_merge(infiles = translate.output, outdir = output.directory, format = "GTiff")
-#   
-# # Enmascar para eliminar pixels fuera del campo
-# clipped.output <- paste0(merge.output, "_masked.tif")
-# sen2r::gdal_warp(srcfiles = merge.output, dstfiles = clipped.output, mask = area.of.interest.utm, overwrite = TRUE)
-# file.rename(from = clipped.output, to = merge.output)
-#   
-#   # Enmascar para eliminar pixels nubosos
-#   # masked.output <- sen2r::s2_mask(infiles = merge.output[1], maskfiles = merge.output[2], 
-#   #                                 mask_type = config$mascara, outdir = config$dir$work, overwrite = TRUE)
-#   
-#   # Determinar la clasificación del suelo
-#   soil.classification <- raster::raster(x = merge.output[2]) %>%
-#     raster::crop(x = ., y = raster::extent(area.of.interest.utm)) %>%
-#     raster::mask(x = ., mask = area.of.interest.utm)
-#   
-#   # Calcular NDWI
-#   ndwi.output <- sen2r::s2_calcindices(infiles = merge.output, indices = "NDWI", overwrite = TRUE, 
-#                                        format = "GTiff", outdir = working.directory, dataType = "Float32")
-#   raster.ndwi <- raster::raster(ndwi.output)
-#   
-#   if (! is.null(ndvi.output)) {
-#     # Transformar a raster y obtener la fecha
-#     raster.campo <- raster::raster(ndvi.output)
-#     fecha        <- as.Date(stringr::str_match(string = names(raster.campo), pattern = "^\\w+_(\\d{8})")[,2], format = "%Y%m%d")
-#     
-#     # Extraer datos dentro del campo y contar la cantidad de NAs
-#     datos.campo   <- unlist(raster::extract(x = raster.campo, y = campo.utm))
-#     porcentaje.na <- 100 * length(which(is.na(datos.campo))) / length(datos.campo)
-#     
-#     if (porcentaje.na < config$maximo.porcentaje.nubes$campo) {
-#       # Guardar raster en base de datos
-#       script$info(paste0("... Guardando datos para fecha ", fecha))
-#       tryCatch({
-#         ano.inicio.campana <- ifelse(lubridate::month(fecha) <= 4, lubridate::year(fecha) - 1, lubridate::year(fecha))
-#         campana            <- paste0(ano.inicio.campana, "-", (ano.inicio.campana+1))
-#         capa.datos.facade$insertarRaster(campo = campo, raster.capa = raster.campo, nombre = 'ndvi', 
-#                                          cultivo = NA, campana = campana, variable = 'NDVI', fecha = fecha)
-#       }, error = function(e) {
-#         script$error(paste0("Error al insertar el raster de NDVI: ", as.character(e)))
-#       })
-#     } else {
-#       script$info(paste0("... Descartando datos por cantidad de nubes ", directorio))
-#     }
-#     
-#     rm(raster.campo, fecha, datos.campo, porcentaje.na)  
-#   } else {
-#     script$info(paste0("... Descartando datos por producir archivo de NDVI nulo ", directorio))
-#   }    
-#   
-#   # Limiar variables
-#   rm(translate.output, merge.output, clipped.output, masked.output, ndvi.output)
-#   gc(full = TRUE)
-# }
-# # ------------------------------------------------------------------------------
--- src/R/sistemas-informacion-geografica/clasificacion/entrenamiento.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-/opt/OTB/bin/otbcli_PolygonClassStatistics  -in    ../tp-teledeteccion2020/images/masked/201801_epsg_22185.tif \
-                                            -vec   ../tp-teledeteccion2020/ground_truth/WaterBodies.shp \
-                                            -field Objeto \
-                                            -out   classes.xml
