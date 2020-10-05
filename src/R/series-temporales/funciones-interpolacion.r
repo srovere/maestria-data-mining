9d500a98717aea54362778aa8fabcc4d7a7d7b7c
Santiago Rovere <srovere@gmail.com>
2020-10-05 13:06:06 -0300

    Uso de descomposicion STL en vez de transformada de Fourier

--- src/R/series-temporales/funciones-interpolacion.r
+++ src/R/series-temporales/funciones-interpolacion.r
@@ -18,18 +18,36 @@ SlowDiscreteFourierTransform <- function(z, inverse = FALSE) {
   vapply(1:n, function(h) sum(z * exp(ff*(h-1)), na.rm = TRUE), complex(1))
 }
 
+# Funcion para decomponer una serie temporal en
+# componentes: estacional (S), tendencia (T) y residuo (R)
+# La serie debe tener la columna fecha (Data) y valor (double)
+DescomponerSerieTemporal <- function(serie.temporal) {
+  # Sealizar decomposicion
+  objeto.stl <- stlplus::stlplus(x = dplyr::pull(serie.temporal, valor),
+                                 t = dplyr::pull(serie.temporal, fecha),
+                                 n.p = 365, s.window = "periodic")
+  
+  # Devolver serie descompuesta
+  serie.descompuesta <- dplyr::bind_cols(
+    serie.temporal,
+    dplyr::select(objeto.stl$data, seasonal, trend, remainder)
+  )
+  return (serie.descompuesta)
+}
+
 
 # Funcion para desestacionalizar una serie temporal
 # Serie temporal es una data frame que al menos tiene
 # las columnas fecha (Date) y valor (double)
-DesestacionalizarSerieTemporal <- function(serie.temporal) {
+DesestacionalizarSerieTemporal <- function(serie.temporal, anos) {
   # Definir filtro para capturar media y frecuencia fundamental
-  N           <- length(unique(serie.temporal$fecha))
-  tiempo      <- seq(from = 1, to = N)
-  ciclos      <- 1
-  filtro      <- rep(0, N)
-  filtro[1:2] <- 1
-  filtro[N]   <- 1
+  N                <- length(unique(serie.temporal$fecha))
+  tiempo           <- seq(from = 1, to = N)
+  ciclos           <- 1
+  filtro           <- rep(0, N)
+  filtro[1]        <- 1
+  filtro[anos+1]   <- 1
+  filtro[N-anos+1] <- 1
   
   # Aplicar descomposicion
   valores <- serie.temporal %>%
@@ -49,35 +67,35 @@ DesestacionalizarSerieTemporal <- function(serie.temporal) {
 # Función para interpolar datos en una fecha determinada
 # Series temporales es una data frame que al menos tiene
 # las columnas omm_id (integer), fecha (Date), valor (double), 
-# envolvente (double) y ruido (double)
+# sesonal (double), trend (double) y remainder (double)
 # Estaciones es un objeto sf de puntos que tiene al menos el campo 
 # omm_id (ID, integer)
 InterpolarValoresFecha <- function(series.temporales, estaciones, fecha) {
   # Obtener ruidos
-  ruidos.fecha <- estaciones %>%
+  residuos.fecha <- estaciones %>%
     dplyr::inner_join(
       dplyr::filter(series.temporales, fecha == !! fecha),
       by = c("omm_id")
     )
 
   # Obtener variograma, ajustar modelo e interpolar
-  locations  <- dplyr::filter(ruidos.fecha, ! is.na(ruido))
-  variograma <- gstat::variogram(ruido ~ 1, data = locations)
-  modelo     <- automap::autofitVariogram(formula = ruido ~ 1, 
+  locations  <- dplyr::filter(residuos.fecha, ! is.na(remainder))
+  variograma <- gstat::variogram(remainder ~ 1, data = locations)
+  modelo     <- automap::autofitVariogram(formula = remainder ~ 1, 
                                           input_data = sf::as_Spatial(locations))
-  kriging    <- gstat::krige(ruido ~ 1, locations = locations, 
-                             newdata = ruidos.fecha, model = modelo$var_model) %>%
+  kriging    <- gstat::krige(remainder ~ 1, locations = locations, 
+                             newdata = residuos.fecha, model = modelo$var_model) %>%
     dplyr::rename(prediccion = var1.pred, varianza = var1.var) %>%
-    dplyr::mutate(omm_id = dplyr::pull(ruidos.fecha, omm_id)) %>%
+    dplyr::mutate(omm_id = dplyr::pull(residuos.fecha, omm_id)) %>%
     dplyr::select(omm_id, prediccion, varianza)
   
   # Hacer join con datos originales y componer nuevamente el valor
   series.interpoladas <- series.temporales %>%
     dplyr::filter(fecha == !! fecha) %>%
-    dplyr::mutate(omm_id = dplyr::pull(ruidos.fecha, omm_id)) %>%
+    dplyr::mutate(omm_id = dplyr::pull(residuos.fecha, omm_id)) %>%
     dplyr::inner_join(kriging, by = c("omm_id")) %>%
-    dplyr::mutate(valor_interpolado = envolvente + 
-                    dplyr::if_else(! is.na(ruido), ruido, prediccion))
+    dplyr::mutate(valor_interpolado = seasonal + trend + 
+                    dplyr::if_else(! is.na(remainder), remainder, prediccion))
 
   return (series.interpoladas)
 }
--- src/R/series-temporales/pipeline-interpolacion.r
+++ src/R/series-temporales/pipeline-interpolacion.r
@@ -8,6 +8,7 @@ require(jsonlite)
 require(purrr)
 require(tidyr)
 require(sf)
+require(stlplus)
 require(stars)
 
 # Cargar funciones necesarias para interpolación espacial
@@ -17,18 +18,21 @@ source("funciones-interpolacion.r", echo = FALSE)
 argentina <- base::readRDS("data/gadm36_ARG_0_sf.rds")
 load("data/Dataset.RData")
 
-# Seleccionamos el año 2018 a efectos ilustrativos. La idea se basa en que hay estaciones más cortas
+# Seleccionamos el período 2001-2010 a efectos ilustrativos. La idea se basa en que hay estaciones más cortas
 # (que o bien arrancaron posteriormente a 1961 o bien se dieron de baja antes de 2019). Para poder 
 # evaluar todas sin descargar las estaciones cortas, se hace un análisis año a año donde se 
 # remueven aquellas estaciones que no cuentan con una mínima cantidad de datos (ver más adelante).
+ano.desde         <- 2001
+ano.hasta         <- 2010
 series.temporales <- datos.estaciones %>%
   dplyr::mutate(fecha = as.Date(fecha)) %>%
-  dplyr::filter(lubridate::year(fecha) == 2018)
+  dplyr::filter(lubridate::year(fecha) %in% seq(from = ano.desde, to = ano.hasta, by = 1))
 
 # Seleccionar series temporales "aceptables"
 # Filtrado: quedan estaciones con mas del 75% de los datos y no mas de 10 dias consecutivos de datos faltantes.
 fechas       <- tidyr::crossing(omm_id = unique(series.temporales$omm_id),
-                                fecha = seq(from = as.Date("2018-01-01"), to = as.Date("2018-12-31"), by = 'days'))
+                                fecha = seq(from = as.Date(sprintf("%d-01-01", ano.desde)), 
+                                            to = as.Date(sprintf("%d-12-31", ano.hasta)), by = 'days'))
 estadisticas <- series.temporales %>%
   dplyr::right_join(fechas, by = c("omm_id", "fecha")) %>%
   dplyr::group_by(omm_id) %>%
@@ -37,7 +41,7 @@ estadisticas <- series.temporales %>%
 
 estaciones.estudio <- estaciones %>%
   dplyr::inner_join(
-    dplyr::filter(estadisticas, (tasa_disponibles >= 0.75) & (max_faltantes_consecutivos <= 10)),
+    dplyr::filter(estadisticas, (tasa_disponibles >= 0.7) & (max_faltantes_consecutivos <= 15)),
     by = c("omm_id"))
 
 series.estudio <- series.temporales %>%
@@ -48,7 +52,7 @@ series.estudio <- series.temporales %>%
 series.descompuestas <- purrr::map_dfr(
   .x = unique(series.estudio$omm_id),
   .f = function(omm_id) {
-    DesestacionalizarSerieTemporal(dplyr::filter(series.estudio, omm_id == !! omm_id))
+    DescomponerSerieTemporal(dplyr::filter(series.estudio, omm_id == !! omm_id))
   }
 )
 
@@ -59,3 +63,5 @@ series.interpoladas <- purrr::map_dfr(
     InterpolarValoresFecha(series.descompuestas, estaciones, fecha)
   }
 )
+
+save(series.interpoladas, file = "data/SeriesInterpoladas.RData")
